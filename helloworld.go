e363ca431b7a30f61b1b904eb5b0fbc4aa33cc30
its-Kevin228 <pekpelignimdoukevin@gmail.com>
2026-03-20 16:58:10 +0000

    api

--- helloworld.go
+++ helloworld.go
@@ -1,58 +1,120 @@
 package main
 
-import {
+import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"strconv"
+)
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
 }
 
-var input1, input2, op string
+var books = []Book{
+	{ID: 1, Title: "1984", Author: "George Orwell", Pages: 328},
+	{ID: 2, Title: "Le Petit Prince", Author: "Antoine de Saint-Exupery", Pages: 96},
+	{ID: 3, Title: "Clean Code", Author: "Robert C. Martin", Pages: 464},
+}
 
-func toFloat64(input string) (float64, error) {
-	return strconv.ParseFloat(input, 64)
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Bienvenue sur mon premier serveur Go HTTP !")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "Kevin"
+	}
+
+	fmt.Fprintln(w, "Bonjour", name, "👋")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Je suis un développeur Go passionné par la création de serveurs web simples et efficaces.")
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Le serveur est en ligne et fonctionne correctement.")
+}
+
+func sumHandler(w http.ResponseWriter, r *http.Request) {
+	a := r.URL.Query().Get("a")
+	b := r.URL.Query().Get("b")
+
+	if a == "" || b == "" {
+		fmt.Fprintln(w, "Veuillez fournir les paramètres 'a' et 'b' pour effectuer la somme.")
+		return
+	}
+
+	aNum, err := strconv.Atoi(a)
+	if err != nil {
+		fmt.Fprintln(w, "Le paramètre 'a' doit être un nombre.")
+		return
+	}
+
+	bNum, err := strconv.Atoi(b)
+	if err != nil {
+		fmt.Fprintln(w, "Le paramètre 'b' doit être un nombre.")
+		return
+	}
+
+	result := aNum + bNum
+	fmt.Fprintln(w, "Le résultat de la somme de", aNum, "et", bNum, "est:", result)
+
+}
+
+func apiStatusHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+		return
+	}
+
+	reponse := map[string]string{
+		"status":  "OK",
+		"message": "Le serveur est en ligne et fonctionne correctement.",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(reponse)
+	if err != nil {
+		http.Error(w, "Erreur lors de l'encodage de la réponse JSON", http.StatusInternalServerError)
+		return
+	}
+}
+
+func apiBooksHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(books)
+	if err != nil {
+		http.Error(w, "Erreur lors de l'encodage JSON", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-
-	fmt.Print("Enter first number: ")
-	fmt.Scanln(&input1)
-
-	fmt.Print("Enter second number: ")
-	fmt.Scanln(&input2)
-
-	fmt.Print("Enter operator (+, -, *, /): ")
-	fmt.Scanln(&op)
-
-	n1,err1:= toFloat64(input1)
-	n2,err2:= toFloat64(input2)
-
-	if err1 != nil {
-		fmt.Println("Invalid input for first number.")
-		return
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/status", statusHandler)
+	http.HandleFunc("/sum", sumHandler)
+	http.HandleFunc("/api/status", apiStatusHandler)
+	http.HandleFunc("/api/books", apiBooksHandler)
+	fmt.Println("Serveur lancé sur http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Erreur serveur:", err)
 	}
-	if err2 != nil {
-		fmt.Println("Invalid input for second number.")
-		return
-	}
-
-	var result float64
-
-	switch op {
-	case "+":
-		result = n1 + n2
-	case "-":
-		result = n1 - n2
-	case "*":
-		result = n1 * n2
-	case "/":
-		if n2 == 0 {
-			fmt.Println("Error: Division by zero.")
-			return
-		}
-		result = n1 / n2
-	default:
-		fmt.Println("Invalid operator.")
-		return
-	}
-
-	fmt.Printf("Result: %.2f\n", result)
 }
